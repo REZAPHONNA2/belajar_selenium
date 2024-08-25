2da35f91ea81384127cd133802d7726edcaa1554
REZAPHONNA2 <muhammadrezaphonna@gmail.com>
2024-08-25 13:11:10 +0700

    FUNGSI HANDLING ALERT (ACCEPT POP UP)

--- belajarselenium.py
+++ belajarselenium.py
@@ -2,15 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+""" (COMMENT) 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach',True)
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 driver.get("https://the-internet.herokuapp.com/windows")
 time.sleep(2)
-"""Menambahkan fungsi button click ke new tab"""
+#Menambahkan fungsi button click ke new tab
 driver.find_element("link text", "Click Here").click()
 time.sleep(2)
-"""fungsi tambah new tab 0=1"""
+fungsi tambah new tab 0=1
 driver.switch_to_window(driver.window_handles[0])
+"""
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get("https://demoqa.com/alerts")
+time.sleep(2)
+driver.find_element(By.ID,"alertButton").click()
+time.sleep(2)
+driver.switch_to.alert.accept()
