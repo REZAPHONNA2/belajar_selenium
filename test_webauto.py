b3cd4c5dcdf91672abd2730c92f55d463f09fa8c
REZAPHONNA2 <muhammadrezaphonna@gmail.com>
2024-09-16 11:41:25 +0700

    MEMBUAT TESTCASE BER PARAMETER

--- test_webauto.py
+++ test_webauto.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
+import pytest
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-def test_bukaGoole():
-    driver.get("https://www.google.com/")
+# fungsi parameterize
+# Buat tupel variabel alamat yg berisi list 
+Alamat = [
+    ("https://www.google.com","Google"),
+    ("https://id-id.facebook.com","Facebook - Masuk atau Daftar")
+]
+
+# ADDRESS = Var Alamat | RESULT = MEMASTIKAN POIN APA YG DI VALIDASI
+# BUAT DECORATOR
+
+@pytest.mark.parametrize('address,result',Alamat)
+                         
+def test_bukaAlamat(address,result):
+    driver.get(address)
     Title = driver.title
 
-    assert Title == "Google"
-
-def test_bukaFB():
-    driver.get("https://id-id.facebook.com/")
-    Title = driver.title
-
-    assert Title == "Facebook - Masuk atau Daftar"    
+    assert Title == result   
